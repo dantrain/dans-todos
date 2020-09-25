7378b5e46811a2c8a9cca32e48177473df88c91f
Dan Train <dan.train@red-badger.com>
2020-09-25 22:01:49 +0100

    Add styling of filter buttons

--- src/client/pages/Home/TodoList/TodoList.tsx
+++ src/client/pages/Home/TodoList/TodoList.tsx
@@ -1,16 +1,20 @@
 import {
+  Button,
+  ButtonGroup,
   Divider,
+  fade,
   List,
   makeStyles,
   Toolbar,
   Typography,
 } from "@material-ui/core";
-import { ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
+import indigo from "@material-ui/core/colors/indigo";
 import React from "react";
 import { graphql, useLazyLoadQuery } from "react-relay/hooks";
+import { NavLink } from "react-router-dom";
 import {
-  TodoListQuery,
   Filter,
+  TodoListQuery,
 } from "../../../__generated__/TodoListQuery.graphql";
 import ClearCompleted from "./ClearCompleted/ClearCompleted";
 import TodoListItem from "./TodoListItem/TodoListItem";
@@ -39,6 +43,10 @@ const useStyles = makeStyles({
   incompleteCount: {
     minWidth: "100px",
   },
+  active: {
+    border: `1px solid ${indigo[500]} !important`,
+    backgroundColor: fade(indigo[500], 0.04),
+  },
 });
 
 type TodoListProps = {
@@ -55,22 +63,38 @@ const TodoList = ({ filter }: TodoListProps) => {
 
   return totalCount && edges ? (
     <>
-      <List>
-        {edges.map(
-          (edge) =>
-            edge?.node && <TodoListItem key={edge.node.id} todo={edge.node} />
-        )}
-      </List>
-      <Divider />
+      {edges.length ? (
+        <>
+          <List>
+            {edges.map(
+              (edge) =>
+                edge?.node && (
+                  <TodoListItem key={edge.node.id} todo={edge.node} />
+                )
+            )}
+          </List>
+          <Divider />
+        </>
+      ) : null}
       <Toolbar className={s.toolbar}>
         <Typography className={s.incompleteCount} color="textSecondary">
           {incompleteCount} item{incompleteCount !== 1 && "s"} left
         </Typography>
-        <ToggleButtonGroup value="all" size="small" exclusive>
-          <ToggleButton value="all">All</ToggleButton>
-          <ToggleButton value="active">Active</ToggleButton>
-          <ToggleButton value="completed">Completed</ToggleButton>
-        </ToggleButtonGroup>
+        <ButtonGroup color="primary" size="small">
+          <Button component={NavLink} to="/" activeClassName={s.active} exact>
+            All
+          </Button>
+          <Button component={NavLink} to="/active" activeClassName={s.active}>
+            Active
+          </Button>
+          <Button
+            component={NavLink}
+            to="/completed"
+            activeClassName={s.active}
+          >
+            Completed
+          </Button>
+        </ButtonGroup>
         <ClearCompleted disabled={incompleteCount === totalCount} />
       </Toolbar>
     </>
