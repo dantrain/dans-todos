20843098d157aecc8c816289212ec43d3d5e263e
Dan Train <dan.train@red-badger.com>
2020-09-27 18:35:02 +0100

    Use switch statement for filter

--- src/server/schemaTypes.ts
+++ src/server/schemaTypes.ts
@@ -27,17 +27,22 @@ export const Query = queryType({
       additionalArgs: {
         filter: arg({ type: Filter, default: "ALL" }),
       },
-      nodes: (_root, { filter }, { prisma }) =>
-        prisma.todo.findMany({
-          orderBy: { created_at: "asc" },
-          ...(filter !== "ALL"
-            ? {
-                where: {
-                  completed: { equals: filter === "COMPLETED" ? true : false },
-                },
-              }
-            : {}),
-        }),
+      nodes: (_root, { filter }, { prisma }) => {
+        switch (filter) {
+          case "ACTIVE":
+            return prisma.todo.findMany({
+              orderBy: { created_at: "asc" },
+              where: { completed: { equals: false } },
+            });
+          case "COMPLETED":
+            return prisma.todo.findMany({
+              orderBy: { created_at: "asc" },
+              where: { completed: { equals: true } },
+            });
+          default:
+            return prisma.todo.findMany({ orderBy: { created_at: "asc" } });
+        }
+      },
       extendConnection: (t) => {
         t.int("totalCount", {
           resolve: (_root, _args, { prisma }) => prisma.todo.count(),
