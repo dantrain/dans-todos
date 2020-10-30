d7027a00274a969e4562499cbf2c8472912bc532
Dan Train <dan.train@red-badger.com>
2020-10-30 17:38:42 +0000

    Remove PORT fallback

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,10 @@ if (module.hot) {
   console.info("✅  Server-side HMR Enabled!");
 }
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-
 export default express()
   .use((req, res) => app.handle(req, res))
-  .listen(port, () => {
-    console.log(`App started on http://localhost:${port}`);
+  .listen(process.env.PORT, () => {
+    console.log(`App started on http://localhost:${process.env.PORT}`);
   })
   .on("error", (error) => {
     console.error(error);
