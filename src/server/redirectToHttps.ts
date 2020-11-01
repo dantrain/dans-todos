9a1b16961ed7a5f709aa7a54496572050498c0fe
Dan Train <dan.train@red-badger.com>
2020-11-01 22:25:06 +0000

    Reverse redirect logic

--- src/server/redirectToHttps.ts
+++ src/server/redirectToHttps.ts
@@ -1,11 +1,11 @@
 import { RequestHandler } from "express";
 
 const redirectToHttps: RequestHandler = (req, res, next) => {
-  if (req.headers["x-forwarded-proto"] === "https") {
-    return next();
+  if (req.protocol === "http") {
+    return res.redirect(`https://${req.hostname}${req.url}`);
   }
 
-  res.redirect(`https://${req.hostname}${req.url}`);
+  next();
 };
 
 export default redirectToHttps;
