66530aa51000c6d6ae47568e1f06ae8c492424ae
Dan Train <dan.train@red-badger.com>
2022-07-25 14:22:06 +0100

    Add pothos

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,6 +1,8 @@
+import { createServer } from "@graphql-yoga/node";
 import express from "express";
 import path from "path";
 import apiRouter from "./api.js";
+import schema from "./schema.js";
 import uiRouter from "./ui.js";
 
 const isProd = process.env.NODE_ENV === "production";
@@ -11,6 +13,10 @@ const getApp = async () => {
   if (isProd) {
     app.use(express.static(path.resolve("dist/client/")));
   }
+
+  const graphQLServer = createServer({ schema });
+
+  app.use("/graphql", graphQLServer);
 
   app.use(apiRouter);
 
---
+++ src/server/schema.ts
@@ -0,0 +1,18 @@
+import SchemaBuilder from "@pothos/core";
+
+const builder = new SchemaBuilder({});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (parent, { name }) => `hello, ${name || "World"}`,
+    }),
+  }),
+});
+
+const schema = builder.toSchema({});
+
+export default schema;
