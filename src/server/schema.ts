1692aa339ab99c025bbb92adb2192cad55e823dd
Dan Train <dan.train@red-badger.com>
2022-07-25 14:53:09 +0100

    Add plugin-relay

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,6 +1,7 @@
 import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from "@pothos/plugin-prisma";
 import type PrismaTypes from "@pothos/plugin-prisma/generated";
+import RelayPlugin from "@pothos/plugin-relay";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({});
@@ -8,16 +9,23 @@ const prisma = new PrismaClient({});
 const builder = new SchemaBuilder<{
   PrismaTypes: PrismaTypes;
 }>({
-  plugins: [PrismaPlugin],
+  plugins: [PrismaPlugin, RelayPlugin],
   prisma: {
     client: prisma,
+  },
+  relayOptions: {
+    clientMutationId: "omit",
+    cursorType: "String",
   },
 });
 
 builder.prismaObject("User", {
   fields: (t) => ({
     id: t.exposeString("id"),
-    todos: t.relation("todos"),
+    todos: t.relatedConnection("todos", {
+      cursor: "id",
+      totalCount: true,
+    }),
   }),
 });
 
