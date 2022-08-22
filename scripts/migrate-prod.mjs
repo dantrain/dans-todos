315b4fafcbdd8bb3098ed8fc51e7171665c77dd6
Dan Train <dan.train@red-badger.com>
2022-08-22 20:24:28 +0100

    Add prod migration script

---
+++ scripts/migrate-prod.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env zx
+import "dotenv/config";
+
+const PROD_DATABASE_PASSWORD = process.env.PROD_DATABASE_PASSWORD;
+
+if (PROD_DATABASE_PASSWORD) {
+  $`flyctl proxy 5432 -a dans-todos-db`;
+
+  await $`wait-on tcp:5432`;
+
+  await $`DATABASE_URL=postgresql://postgres:${PROD_DATABASE_PASSWORD}@localhost:5432/postgres yarn prisma migrate deploy`;
+
+  // Kill process listening on port 5432
+  $`lsof -t -i tcp:5432 | xargs kill`;
+} else {
+  console.log("Set PROD_DATABASE_PASSWORD env var");
+}
