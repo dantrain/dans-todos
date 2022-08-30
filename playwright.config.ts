ebe13d35b0be44a91fd95e840701841923c7ecf3
Dan Train <dan.train@red-badger.com>
2022-08-30 15:09:02 +0100

    Update workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -103,10 +103,11 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: "yarn start:prod",
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
 };
 
 export default config;
